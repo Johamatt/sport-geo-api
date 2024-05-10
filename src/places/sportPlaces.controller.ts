6e34b90ca5e5af7d8438dbc8e3172ec77c8579ae
johamatt94 <johamatt94@gmail.com>
2024-05-11 02:50:13 +0300

    return nearby places with distance + fixed findAll query

--- src/places/sportPlaces.controller.ts
+++ src/places/sportPlaces.controller.ts
@@ -18,19 +18,39 @@ export class PlacesController {
   @ApiQuery({ name: 'latitude', required: true, example: 60.240691 })
   @ApiQuery({ name: 'longitude', required: true, example: 24.847974 })
   @ApiQuery({ name: 'radius', required: true, example: 500 })
+  @ApiQuery({ name: 'page', required: false, example: 1 })
+  @ApiQuery({ name: 'limit', required: false, example: 10 })
   @Get('nearby')
   async findNearby(
     @Query('latitude') latitude: number,
     @Query('longitude') longitude: number,
     @Query('radius') radius: number,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
   ): Promise<SportPlace[]> {
     try {
+      const offset = (page - 1) * limit;
       const nearbyPlaces = await SportPlace.findAll({
+        attributes: [
+          'id',
+          'name',
+          'type',
+          [
+            Sequelize.literal(`CAST(ST_Distance(
+              "SportPlace"."geom",
+              ST_SetSRID(ST_MakePoint(${longitude}, ${latitude}), 4326)::geography
+            ) AS INTEGER)`),
+            'distance',
+          ],
+        ],
         where: Sequelize.literal(`ST_DWithin(
           "SportPlace"."geom",
-          ST_MakePoint(${longitude}, ${latitude})::geography,
+          ST_SetSRID(ST_MakePoint(${longitude}, ${latitude}), 4326)::geography,
           ${radius}
         )`),
+        order: Sequelize.literal('distance ASC'),
+        offset,
+        limit,
       });
 
       return nearbyPlaces;
@@ -40,20 +60,21 @@ export class PlacesController {
     }
   }
 
-  @ApiParam({ name: 'id', type: 'string', example: '1' })
-  @Get(':id')
-  async findById(@Param('id') id: Identifier): Promise<SportPlace> {
-    return this.sportPlace.findByPk(id);
-  }
-
   @ApiQuery({ name: 'page', required: true, example: 1 })
   @ApiQuery({ name: 'perPage', required: true, example: 10 })
+  @ApiQuery({ name: 'type', required: false, example: 'Kuntosali' })
   @Get()
   async findAll(
     @Query('page') page: number = 1,
     @Query('perPage') perPage: number = 10,
+    @Query('type') type?: string,
   ): Promise<{ sportPlaces: SportPlace[]; total: number }> {
+    let whereClause = {};
+    if (type) {
+      whereClause = { type };
+    }
     const { count, rows } = await this.sportPlace.findAndCountAll({
+      where: whereClause,
       limit: perPage,
       offset: (page - 1) * perPage,
     });
